35fbc64e00c1befbd050a90a3d604af8d8871f40
Natasha SQ <nt.zecevic@gmail.com>
2021-07-11 14:30:32 +0200

    update socket cors  origin

--- server.js
+++ server.js
@@ -51,7 +51,7 @@ app.use(
     resave: false,
     saveUninitialized: false,
     cookie: {
-      sameSite: 'none',
+      sameSite: "none",
       secure: true,
       maxAge: 1000 * 60 * 60 * 24,
     },
@@ -88,7 +88,10 @@ app.use(userRoute);
 const io = (path) =>
   require("socket.io")(server, {
     cors: {
-      origin: "http://localhost:3000",
+      origin: [
+        "http://localhost:3000",
+        "https://socializard-client.herokuapp.com",
+      ],
     },
     path,
   });
